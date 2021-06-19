b0fd727b4091492f10ae11b4ed011058e6a96c6a
Nichi <nichisenpai@gmail.com>
2021-06-19 19:33:17 +0200

    Snipe bot operational waiting for tests

--- src/snipe.js
+++ src/snipe.js
@@ -31,18 +31,18 @@ let router = new ethers.Contract(
     addresses.router,
     [
       'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
-      'function swapExactTokensForETHSupportingFeeOnTransferTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
+      'function swapExactTokensForTokensSupportingFeeOnTransferTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
     ],
     signer
   );
   
 // FUNCTION TO SNIPE TOKEN
 async function snipeToken (_tokenToSwap, _minLiquidityBeforeBuy, _bnbAmountToSpend, _inputGas) {
-  const addressSigner = await signer.getAddress();
-  const minLiquidityOfToken = _minLiquidityBeforeBuy // IN BNB
-  const amountToBuy = _bnbAmountToSpend // in WBNB
-  let jmlBnb = 0;
   let initialLiquidityDetected = false;
+  const addressSigner = await signer.getAddress();
+  const pairAddress = await factory.getPair(addresses.WBNB, _tokenToSwap);
+  const _gasPrice = ethers.utils.parseUnits(_inputGas,'gwei')
+  const amountToBuy = _bnbAmountToSpend // in WBNB
   // Setting up BNB
   const wbnb = new ethers.Contract(
     _tokenToSwap,
@@ -52,90 +52,57 @@ async function snipeToken (_tokenToSwap, _minLiquidityBeforeBuy, _bnbAmountToSpe
     signer
   );
   // Check liquidity of a pair
-  const run = async () => { await checkLiq();}
-  let checkLiq = async() => {
-    console.log('test');
-    const pairAddressx = await factory.getPair(_tokenToSwap, addresses.WBNB);
-    console.log(`pairAddress: ${pairAddressx}`);
-    if (pairAddressx !== null && pairAddressx !== undefined) {
-      if (pairAddressx.toString().indexOf('0x0000000000000') > -1) {
-        console.log(`pairAddress ${pairAddressx} not detected. Auto restart`);
-        return await run();
-      }
-    }
-    const pairBNBvalue = await wbnb.balanceOf(pairAddressx); 
-    jmlBnb = await ethers.utils.formatEther(pairBNBvalue);
-    console.log(`value BNB : ${jmlBnb}`);
-  
-    if(jmlBnb > minLiquidityOfToken){
-        setTimeout(() => buyAction(), 3000);
-    }
-    else{
-        initialLiquidityDetected = false;
-        console.log(' run again...');
-        return await run();
-      }
-  }
-  //const amountIn = ethers.utils.parseUnits(_tokenAmount, _tokenDecimals);
-  const _gasPrice = ethers.utils.parseUnits(_inputGas,'gwei')
-
-  let buyAction = async() => {
+  const pair = new ethers.Contract(
+    pairAddress, 
+    ['event Mint(address indexed sender, uint amount0, uint amount1)']
+    , signer);
+  pair.on('Mint', async (sender, amount0, amount1) => {
     if(initialLiquidityDetected === true) {
-      console.log('not buy cause already buy');
-        return null;
+      return;
     }
-    
-    console.log('ready to buy');
-    try{
-      initialLiquidityDetected = true;
-
-      let amountOutMin = 0;
-      //We buy x amount of the new token for our wbnb
-      const amountIn = ethers.utils.parseUnits(`${amountToBuy}`, 'ether');
-   
-      console.log(
-       `Start to buy \n`
-        +
-        `Buying Token
-        =================
-        tokenIn: ${(amountIn * 1e-18).toString()} ${addresses.WBNB} (BNB)
-        tokenOut: ${amountOutMin.toString()} ${_tokenToSwap}
-      `);
-     
-      console.log('Processing Transaction.....');
-      console.log(`amountIn: ${(amountIn * 1e-18)} ${addresses.WBNB} (BNB)`);
-      console.log(`amountOutMin: ${amountOutMin}`);
-      console.log(`tokenIn: ${addresses.WBNB}`);
-      console.log(`tokenOut: ${_tokenToSwap}`);
-      console.log(`data.recipient: ${addressSigner}`);
-      console.log(`data.gasLimit: ${2000000}`);
-      console.log(`data.gasPrice: ${_gasPrice}`);
-
-      const tx = await router.swapExactTokensForTokensSupportingFeeOnTransferTokens( 
-        amountIn,
-        amountOutMin,
-        [addresses.WBNB, _tokenToSwap],
-        addressSigner,
-        Date.now() + 1000 * 60 * 5, //5 minutes
-        {
-          'gasLimit': 2000000,
-          'gasPrice': _gasPrice
-      });
-     
-      const receipt = await tx.wait(); 
-      console.log(`Transaction receipt : https://www.bscscan.com/tx/${receipt.logs[1].transactionHash}`);
-      setTimeout(() => {process.exit()},2000);
-    }catch(err){
-      let error = JSON.parse(JSON.stringify(err));
-        console.log(`Error caused by : 
-        {
-        reason : ${error.reason},
-        transactionHash : ${error.transactionHash}
-        message : Please check your BNB/WBNB balance, maybe its due because insufficient balance or approve your token manually on pancakeSwap
-        }`);
-        console.log(error);
-    }
+    initialLiquidityDetected = true;
+    console.log(
+      `Liquidity Addition Detected\n`
+      +
+      `Details
+      =================
+      sender: ${sender}
+      amount0: ${amount0}
+      amount1:${amount1}
+    `);
+    const pairBNBvalue = await wbnb.balanceOf(pairAddress);
+    let rdblValue = await ethers.utils.formatEther(pairBNBvalue);
+    const amountIn = ethers.utils.parseUnits(`${amountToBuy}`, 'ether');
+    // Buy Token on liquidity add
+    console.log(
+      `Buying Token\n`
+      +
+      `Details
+      =================
+      amountIn: ${amountIn} BNB
+      GasPrice : ${_gasPrice} GWEI
+      PairBNBValue : ${rdblValue}
+      PairAddress : ${pairAddress}
+      AdressSigner : ${addressSigner}
+    `);
+    if(rdblValue > _minLiquidityBeforeBuy){
+      setTimeout(async function(){
+        const tx = await router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+          amountIn,
+          0,
+          [addresses.WBNB, _tokenToSwap],
+          addressSigner,
+          Date.now() + 1000 * 60 * 10, //10 minutes
+          {
+            'gasLimit': 2000000,
+            'gasPrice': _gasPrice,
+            'nonce' : null
+        });
+        const receipt = await tx.wait(); 
+        console.log(`Transaction receipt : https://www.bscscan.com/tx/${receipt.transactionHash}`);
+      }, 3000);
   }
+  });
 }
 module.exports = {
   snipeToken
